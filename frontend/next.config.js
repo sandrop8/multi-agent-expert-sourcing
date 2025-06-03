c09ccae8c3e06b5329cfc832859f62c113311489
jbp <jensbosseparra@gmx.de>
2025-06-03 02:42:49 +0200

    Fix Next.js path alias resolution for Docker builds

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,11 +7,22 @@ const nextConfig = {
     ignoreBuildErrors: false,
   },
   // Configure webpack for path aliases
-  webpack: (config) => {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Get the absolute path to the project root
+    const projectRoot = path.resolve(__dirname)
+
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': path.resolve(__dirname),
+      '@': projectRoot,
+      '@/lib': path.join(projectRoot, 'lib'),
+      '@/lib/utils': path.join(projectRoot, 'lib', 'utils'),
+      '@/components': path.join(projectRoot, 'components'),
+      '@/app': path.join(projectRoot, 'app'),
     }
+
+    // Debug: uncomment the line below to see path resolution in build logs
+    // console.log('Webpack alias configuration:', config.resolve.alias)
+
     return config
   },
 }
